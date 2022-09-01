1f169fc7f09cb8c8f8e61ea49b5ba020ff944609
dewisapp <dew.fitri2@gmail.com>
2022-09-01 14:33:50 +0700

    commit

---
+++ src/components/layout/Header/index.tsx
@@ -0,0 +1,31 @@
+import Container from "components/Container";
+import Image from "next/Image";
+import traveloLogo from "public/assets/travelo-logo-header.png";
+import Nav from "./Nav";
+import Search from "./Search";
+
+const Header = () => {
+  return (
+    <header className="h-[85px] bg-white shadow-lg p-4">
+      <Container>
+        <div className="flex justify-between items-center">
+          <div className="flex">
+            <Image
+              src={traveloLogo}
+              alt="Travelo Logo"
+              placeholder="blur"
+              height="48"
+              width="182"
+            />
+            <Search />
+          </div>
+          <div className="flex">
+            <Nav />
+          </div>
+        </div>
+      </Container>
+    </header>
+  );
+};
+
+export default Header;
